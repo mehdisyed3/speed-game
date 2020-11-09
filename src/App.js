4c2b7a6710563a539bc917885f441e74951fac44
Syed Mehdi <mehdisyed3@gmail.com>
2020-11-08 19:32:28 -0500

    complete speed typing game

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
+  const TIME = 5
+  const [text, setText] = useState('')
+  const [timeRemaining, setTimeRemaining] = useState(TIME)
+  const [isGameOn, setIsGameOn] = useState(false)
+
+  const textRef = useRef(null)
+
+  function handleChange(event) {
+    const { value } = event.target
+    setText(value)
+  }
+
+  function startGame() {
+    setIsGameOn(true)
+    setTimeRemaining(TIME)
+    setText('')
+    textRef.current.disabled = false
+    textRef.current.focus()
+  }
+
+  useEffect(() => {
+    if (timeRemaining > 0 && isGameOn) {
+      setTimeout(() => setTimeRemaining(timeRemaining - 1), 1000)
+    }
+    else {
+      setIsGameOn(false)
+    }
+  }, [timeRemaining, isGameOn])
+
+  const wordCount = text.trim().split(" ")
+    .filter(item => item !== "")
+    .length
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+
+      <h1>How fast can you type? </h1>
+
+      <textarea ref={textRef}
+        disabled={!isGameOn}
+        onChange={handleChange}
+        value={text}
+      />
+
+      <h1>Time Remaining:{timeRemaining} </h1>
+
+      <button disabled={isGameOn} onClick={() => startGame()}>Start Game</button>
+
+      <h1>Word Count:{!isGameOn && wordCount} </h1>
+
     </div>
   );
 }
